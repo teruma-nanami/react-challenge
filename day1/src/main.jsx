07b5858b7977a6073f27bdb7e0854e2020b14600
teruma-nanami <kinos.travel.fun@gmail.com>
2025-11-01 17:48:40 +0900

    build dist

--- day1/src/main.jsx
+++ day1/src/main.jsx
@@ -18,20 +18,20 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/day1">
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/router" element={<ReactRouter />} />
-      <Route path="/state" element={<StateDemo />} />
-      <Route path="/state-name" element={<StateNameDemo />} />
-      <Route path="/state-toggle" element={<StateToggleDemo />} />
-      <Route path="/state-todo" element={<StateTodoDemo />} />
-      <Route path="/state-user" element={<StateUserDemo />} />
-      <Route path="/state-styled-toggle" element={<StateStyledToggleDemo />} />
-      <Route path="/state-multi" element={<StateMultiDemo />} />
-      <Route path="/state-reset" element={<StateResetDemo />} />
+      <Route path="router" element={<ReactRouter />} />
+      <Route path="state" element={<StateDemo />} />
+      <Route path="state-name" element={<StateNameDemo />} />
+      <Route path="state-toggle" element={<StateToggleDemo />} />
+      <Route path="state-todo" element={<StateTodoDemo />} />
+      <Route path="state-user" element={<StateUserDemo />} />
+      <Route path="state-styled-toggle" element={<StateStyledToggleDemo />} />
+      <Route path="state-multi" element={<StateMultiDemo />} />
+      <Route path="state-reset" element={<StateResetDemo />} />
       <Route
-        path="/state-conditional"
+        path="state-conditional"
         element={<StateConditionalRenderDemo />}
       />
-      <Route path="/state-history" element={<StateHistoryDemo />} />
+      <Route path="state-history" element={<StateHistoryDemo />} />
     </Routes>
   </BrowserRouter>
 );
