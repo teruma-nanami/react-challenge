9921d368ba566aa32062e35a53a6006d73452809
teruma-nanami <kinos.travel.fun@gmail.com>
2025-11-03 07:44:41 +0900

    useEffect

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ChildArea } from "./components/ChildArea";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
 
@@ -10,11 +11,31 @@ function App() {
     setOpen(!open);
   };
 
+  const onClickAdd = () => {
+    setCount(count + 1);
+  };
+
   const onChangeInput = (e) => {
     setText(e.target.value);
   };
+
+  useEffect(() => {
+    if (count > 0) {
+      if (count % 3 === 0) {
+        setOpen(true);
+      } else {
+        setOpen(false);
+      }
+    }
+    console.log("カウントが更新されました");
+  }, [count]);
+
   return (
     <>
+      <p>カウント: {count}</p>
+      <button onClick={onClickAdd}>カウントアップ</button>
+      <br />
+      <br />
       <input type="text" value={text} onChange={onChangeInput} />
       <br />
       <br />
--- day3/src/components/ChildArea.jsx
+++ day3/src/components/ChildArea.jsx
@@ -6,5 +6,15 @@ const style = {
 
 export const ChildArea = (props) => {
   const { open } = props;
+  // const data = [...Array(2000).keys()]; // 0から1999までの配列
+  // data.forEach(() => {
+  //   console.log("レンダリングされました");
+  // });
+
+  const data = [...Array(1).keys()]; // 0から1999までの配列
+  data.forEach(() => {
+    console.log("useEffectの練習");
+  });
+
   return <>{open ? <div style={style}>子コンポーネント</div> : null}</>;
 };
