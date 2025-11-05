426b19ffda045bd66301128dbdaba6d5fa49b600
teruma-nanami <kinos.travel.fun@gmail.com>
2025-11-05 13:54:30 +0900

    make dist

--- day5/src/practices/Practice3.tsx
+++ day5/src/practices/Practice3.tsx
@@ -1,20 +1,20 @@
 export const Practice3 = () => {
   // 練習問題2: 戻り値の型指定
   // ここではnumber型の値を返すように設定している
-  const getTotalfee = (num: number)=> {
+  const getTotalfee = (num: number) => {
     const total = num * 1.3;
     return total.toString();
-  }
+  };
   const onclickPractice = () => {
-    let total: number = 0;
+    let total: string = "0";
     total = getTotalfee(1000);
     console.log(`合計金額は${total}円です`);
-  }
+  };
 
   return (
     <div>
       <p>練習問題:変数の型指定</p>
       <button onClick={onclickPractice}>練習問題3を実行</button>
     </div>
-  )
-}
+  );
+};
