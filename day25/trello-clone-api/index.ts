b320b7300a2a6d83680f54df4aa5750b944240c5
teruma-nanami <kinos.travel.fun@gmail.com>
2025-11-27 14:28:40 +0900

    make database table

--- day25/trello-clone-api/index.ts
+++ day25/trello-clone-api/index.ts
@@ -1,12 +1,18 @@
+import cors from "cors";
 import express from "express";
+import { AppDataSource } from "./datasource";
 
 const app = express();
 const PORT = 8888;
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+AppDataSource.initialize().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
