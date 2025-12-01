6a3cd3c61def192995105081acce389554dd885f
teruma-nanami <kinos.travel.fun@gmail.com>
2025-12-01 10:11:06 +0900

    installed laravel and react

---
+++ day31/src/resources/js/app.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return <h1>Hello React × Laravel!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
---
+++ day31/src/resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Laravel × React</title>
+  @viteReactRefresh
+  @vite('resources/js/app.jsx')
+</head>
+
+<body>
+  <div id="app"></div>
+</body>
+
+</html>
