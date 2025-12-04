70dee907ed320d346746c48ddd339b16bfe39c7e
teruma-nanami <kinos.travel.fun@gmail.com>
2025-12-04 17:50:02 +0900

    make dist

--- day33/src/main.tsx
+++ day33/src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/day33/">
         <App />
       </BrowserRouter>
     </ChakraProvider>
