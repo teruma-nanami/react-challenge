8dc39a53abf20664df96bb942bcd9e3f7c44aa13
nanami-teruma <kinos.travel.fun@gmail.com>
2026-01-30 10:51:19 +0900

    make middleware

--- officeapp/backend/routes/api.php
+++ officeapp/backend/routes/api.php
@@ -14,42 +14,41 @@ use Illuminate\Support\Facades\Route;
 Route::post('/contacts', [ContactController::class, 'store']);
 
 // 認証必須：管理（一覧/詳細/更新/削除）
-// Route::middleware('auth')->group(function () {
-Route::get('/contacts', [ContactController::class, 'index']);
-Route::get('/contacts/{id}', [ContactController::class, 'show']);
-Route::put('/contacts/{id}', [ContactController::class, 'update']);
-Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
+Route::middleware('auth0')->group(function () {
+    Route::get('/contacts', [ContactController::class, 'index']);
+    Route::get('/contacts/{id}', [ContactController::class, 'show']);
+    Route::put('/contacts/{id}', [ContactController::class, 'update']);
+    Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
 
-Route::get('/tasks', [TaskController::class, 'index']);
-Route::post('/tasks', [TaskController::class, 'store']);
-Route::put('/tasks/{id}', [TaskController::class, 'update']);
-Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::put('/tasks/{id}', [TaskController::class, 'update']);
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
 
-Route::apiResource('items', ItemController::class);
-// Route::get('/items', [ItemController::class, 'index']);
-// Route::get('/items/{id}', [ItemController::class, 'show']);
-// Route::post('/items', [ItemController::class, 'store']);
-// Route::put('/items/{id}', [ItemController::class, 'update']);
-// Route::delete('/items/{id}', [ItemController::class, 'destroy']);
+    Route::apiResource('items', ItemController::class);
+    // Route::get('/items', [ItemController::class, 'index']);
+    // Route::get('/items/{id}', [ItemController::class, 'show']);
+    // Route::post('/items', [ItemController::class, 'store']);
+    // Route::put('/items/{id}', [ItemController::class, 'update']);
+    // Route::delete('/items/{id}', [ItemController::class, 'destroy']);
 
-Route::apiResource('transactions', TransactionController::class)
-    ->only(['index', 'show', 'store']);
-// Route::get('/transactions', [TransactionController::class, 'index']);
-// Route::get('/transactions/{id}', [TransactionController::class, 'show']);
-// Route::post('/transactions', [TransactionController::class, 'store']);
-// });
+    Route::apiResource('transactions', TransactionController::class)
+        ->only(['index', 'show', 'store']);
+    // Route::get('/transactions', [TransactionController::class, 'index']);
+    // Route::get('/transactions/{id}', [TransactionController::class, 'show']);
+    // Route::post('/transactions', [TransactionController::class, 'store']);
+    // });
 
-Route::post('/attendances/check-in', [AttendanceController::class, 'checkIn']);
-Route::post('/attendances/check-out', [AttendanceController::class, 'checkOut']);
-Route::get('/attendances/today', [AttendanceController::class, 'today']);
-Route::get(
-    '/attendances/{attendanceId}/break-times',
-    [BreakTimeController::class, 'indexByAttendance']
-);
-Route::post('/break-times/start', [BreakTimeController::class, 'start']);
-Route::put('/break-times/{id}/end', [BreakTimeController::class, 'end']);
-Route::get('/profile', [UserController::class, 'me']);
-Route::put('/profile', [UserController::class, 'update']);
-Route::post('/auth/create', [UserController::class, 'create']);
-
-// }
+    Route::post('/attendances/check-in', [AttendanceController::class, 'checkIn']);
+    Route::post('/attendances/check-out', [AttendanceController::class, 'checkOut']);
+    Route::get('/attendances/today', [AttendanceController::class, 'today']);
+    Route::get(
+        '/attendances/{attendanceId}/break-times',
+        [BreakTimeController::class, 'indexByAttendance']
+    );
+    Route::post('/break-times/start', [BreakTimeController::class, 'start']);
+    Route::put('/break-times/{id}/end', [BreakTimeController::class, 'end']);
+    Route::get('/profile', [UserController::class, 'me']);
+    Route::put('/profile', [UserController::class, 'update']);
+    Route::post('/auth/create', [UserController::class, 'create']);
+});
